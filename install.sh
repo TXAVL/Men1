42ddb46cdda837f8081c1bbe18444c15ccbfe228
TXAVL <102934765+TXAVL@users.noreply.github.com>
2024-09-13 13:44:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -432,6 +432,7 @@ show_usage() {
     echo -e "5. Xem changelog: Xem lịch sử các thay đổi của script."
     echo -e "6. Hướng dẫn sử dụng: Hiển thị thông tin này."
     echo -e "7. Thoát: Kết thúc script."
+    echo -e "8. Hãy nhớ bạn có thể mua key tại https..... hoặc ib qua fb: https://bom.so/FB_ADMIN!"
     echo -e "\nLưu ý: Đảm bảo bạn có kết nối internet ổn định khi sử dụng script này."
 }
 
